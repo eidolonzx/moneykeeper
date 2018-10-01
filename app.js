8af568f6825747e633c5332d429a5c9f993d5202
eidolonzx <eidolonzx@gmail.com>
2018-10-01 14:21:15 +0400

    Added DOMstrings to UI controller

--- app.js
+++ app.js
@@ -4,16 +4,50 @@ var budgetController = (function(){
 
 var UIController = (function(){
 
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+
 })();
 
 var controller = (function(budgetCtrl, UICtrl){
+
+  var DOM = UIController.getDOMstrings();
+
   var ctrlAddItem = function() {
-    // добавляем пункт списка
-    console.log('It works!');
+    // 1. Получаем данные из поля ввода
+    var input = UIController.getInput();
+    console.log(input);
+
+    // 2. Добавляем объект в budget controller
+
+    // 3. Добавляем объект в UI
+
+    // 4. Пересчитываем бюджет
+
+    // 5. Отображаем итоговый бюджет в UI
+
   };
 
   // слушаем клик мышки
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   // слушаем нажатие ENTER
   document.addEventListener('keypress', function(event) {
