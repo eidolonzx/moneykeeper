81590bc71a884d965a79b4aeedc9570a1c1b1b6a
eidolonzx <eidolonzx@gmail.com>
2018-10-03 18:59:39 +0400

    added addListItem function to UI

--- app.js
+++ app.js
@@ -61,7 +61,9 @@ var UIController = (function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expencesContainer: '.expenses__list'
   }
 
   return {
@@ -71,6 +73,25 @@ var UIController = (function() {
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       };
+    },
+
+    addListItem: function(obj, type) {
+        var html, newHtml, element;
+        if (type === 'inc') {
+          element = DOMstrings.incomeContainer;
+
+          html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        } else if (type === 'exp') {
+          element = DOMstrings.expencesContainer;
+
+          html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        }
+
+        newHtml = html.replace('%id%', obj.id);
+        newHtml = newHtml.replace('%description%', obj.description);
+        newHtml = newHtml.replace('%value%', obj.value);
+
+        document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
     getDOMstrings: function() {
@@ -103,6 +124,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Добавляем объект в UI
+    UICtrl.addListItem(newItem, input.type);
 
     // 4. Пересчитываем бюджет
 
