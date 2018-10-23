96bae51642c51136ef0bd24df1c5dc625be59cb0
eidolonzx <eidolonzx@gmail.com>
2018-10-23 19:09:23 +0400

    added calculate-budget function

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ var budgetController = (function() {
     this.value = value;
   }
 
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(cur) {
+      sum += cur.value;
+    });
+    data.totals[type] = sum;
+  }
+
   var data = {
     allItems: {
       exp: [],
@@ -20,7 +28,9 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   }
 
   return {
@@ -46,6 +56,31 @@ var budgetController = (function() {
       data.allItems[type].push(newItem);
       return newItem;
 
+    },
+
+    calculateBudget: function() {
+      // calculate total income and expences
+      calculateTotal('exp');
+      calculateTotal('inc');
+
+      // calculete the budget: income - expence
+      data.budget = data.totals.inc - data.totals.exp;
+
+      // calculate the percentage of income that we spent
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+    },
+
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      }
     },
 
     testing: function() {
@@ -128,13 +163,15 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
   }
 
-  var updateBudget = function () {
-    // 1. Пересчитываем бюджет
+  var updateBudget = function() {
+    // 1. calculate the budget
+    budgetCtrl.calculateBudget();
 
     // 2. return the budget
+    var budget = budgetCtrl.getBudget();
 
     // 3. Отображаем итоговый бюджет в UI
-
+    console.log(budget);
   };
 
   var ctrlAddItem = function() {
